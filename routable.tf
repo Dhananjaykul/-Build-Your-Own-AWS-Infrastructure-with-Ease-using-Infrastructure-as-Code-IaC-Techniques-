600743705e1b66774d8a21539b0a723f652b2715
Dhananjay Kulkarni <125235866+Dhananjaykul@users.noreply.github.com>
2023-06-05 18:37:51 +0530

    Create routable.tf

---
+++ routable.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
